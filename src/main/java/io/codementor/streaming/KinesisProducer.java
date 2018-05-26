0cac60433024c578d19c10bb5b8f81f49e1a5abf
Ivan Mushketyk <ivan.mushketik@gmail.com>
2018-05-26 17:04:28 +0100

    Add more comments

--- src/main/java/io/codementor/streaming/KinesisProducer.java
+++ src/main/java/io/codementor/streaming/KinesisProducer.java
@@ -12,27 +12,40 @@ import static io.codementor.streaming.Utils.STREAM_NAME;
 import static io.codementor.streaming.Utils.sleep;
 import static io.codementor.streaming.Utils.toBytes;
 
+/**
+ * Writes metrics to a Kinesis stream.
+ */
 public class KinesisProducer {
 
+    // In a real application we would collect metrics from multiple
+    // machines and write them to the same Kinesis stream.
+    // For demo purposes we emulate multiple hosts writing
+    // to the same stream by using several threads.
     private static final int NUM_OF_HOSTS = 4;
 
     public static void main(String[] args) {
 
         for (int i = 0; i < NUM_OF_HOSTS; i++) {
+            // Set a "host" name
             String hostName = "host-" + i;
             System.out.println("Launching producer for host: " + hostName);
+            // Create a thread for writing data into a stream
             Thread thread = new Thread(new MetricsProducer(hostName));
             thread.start();
         }
     }
 }
 
+/**
+ * A single producer thread
+ */
 class MetricsProducer implements Runnable {
-    
+
+    // Used to get current OS metrics
     private static OperatingSystemMXBean OS_BEAN = ManagementFactory.getOperatingSystemMXBean();
 
     private final String hostName;
-    
+
     private final Random random = new Random();
 
     public MetricsProducer(String hostName) {
@@ -43,18 +56,23 @@ class MetricsProducer implements Runnable {
     public void run() {
         AmazonKinesis client = Utils.createKinesisClient();
 
+        // Store metrics in an infinite loop
         while (true) {
+            // Get current CPU load metric
             Metric metric = getCpuLoadMetric();
 
             PutRecordRequest putRecordRequest = new PutRecordRequest();
             // Set a stream name. The same as we created in previous example
             putRecordRequest.setStreamName(STREAM_NAME);
             // Set metric name as a key of the new record
+            // Not all metrics will be sent to the same shard
+            // since metrics from different "hosts" will have keys
             putRecordRequest.setPartitionKey(metric.getMetricName());
             // Finally we specify data that we want to store
             putRecordRequest.setData(toBytes(metric));
 
             try {
+                // Store a record with a single metric value
                 client.putRecord(putRecordRequest);
             } catch (AmazonClientException ex) {
                 System.out.println("Failed to write Kinesis record");
@@ -65,10 +83,14 @@ class MetricsProducer implements Runnable {
     }
 
     private Metric getCpuLoadMetric() {
+        // Get system load metric and change it randomly for different processors
+        // to avoid writing same value for different metrics
         double load = OS_BEAN.getSystemLoadAverage() + random.nextDouble();
+        // Create a metric name
         String metricName = hostName + "-system-load";
         System.out.println(String.format("System load for %s: %f", metricName, load));
 
+        // Create a metric instance
         return new Metric(metricName, load);
     }
 }
--- src/main/java/io/codementor/streaming/Metric.java
+++ src/main/java/io/codementor/streaming/Metric.java
@@ -1,9 +1,21 @@
 package io.codementor.streaming;
 
+/**
+ * A class that represent a single metric value. The producer application
+ * is writing serialized instances of this class to a Kinesis stream
+ * and producers are reading stored records and deserialize them into
+ * instances of this class.
+ *
+ * Every metric is a pair of a metric name and a double value.
+ *
+ * In a real application this class would be more complicated and could
+ * additional information such as timestamp, host, aggregated statistics, etc.
+ */
 public class Metric {
     private String metricName;
     private double value;
 
+    // Used by Jackson library during deserialization
     public Metric() {
 
     }
